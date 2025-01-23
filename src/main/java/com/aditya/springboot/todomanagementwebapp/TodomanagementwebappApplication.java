6a2911a1b9cdb25eb04f399abb9a44236851415c
Aditya-Prakash07 <prakashaditya775@gmail.com>
2025-01-23 17:14:36 +0530

    initial commit

---
+++ src/main/java/com/aditya/springboot/todomanagementwebapp/TodomanagementwebappApplication.java
@@ -0,0 +1,13 @@
+package com.aditya.springboot.todomanagementwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodomanagementwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodomanagementwebappApplication.class, args);
+	}
+
+}
