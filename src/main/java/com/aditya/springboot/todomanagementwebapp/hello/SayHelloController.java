294007e67834f3dca08ca4ce7b47b0b71e30c28d
Aditya-Prakash07 <prakashaditya775@gmail.com>
2025-01-24 14:24:23 +0530

    say hello using html ..

--- src/main/java/com/aditya/springboot/todomanagementwebapp/hello/SayHelloController.java
+++ src/main/java/com/aditya/springboot/todomanagementwebapp/hello/SayHelloController.java
@@ -6,14 +6,36 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller // this annotaion is for showing that below class is something that handles web-request
+//public class SayHelloController {
+//    //"say-hello" => "Hello! What are you learning today?"
+//    //say-hello ---> i want that when someone clicks on my url named say-hello he will get the above message
+//    // url ==>  http://localhost:8080/say-hello
+//    @RequestMapping("say-hello") // map to url
+//    @ResponseBody // using this annotation we are sending the string returned here as response
+//    public String sayHello(){
+//        return "Hello! What are you learning today?";
+//    }
+//
+//}
+
 public class SayHelloController {
     //"say-hello" => "Hello! What are you learning today?"
     //say-hello ---> i want that when someone clicks on my url named say-hello he will get the above message
     // url ==>  http://localhost:8080/say-hello
-    @RequestMapping("say-hello") // map to url
+    @RequestMapping("say-hello-html") // map to url
     @ResponseBody // using this annotation we are sending the string returned here as response
-    public String sayHello(){
-        return "Hello! What are you learning today?";
+    public String sayHelloHtml(){
+        StringBuffer sb = new StringBuffer();
+        sb.append("<html>");
+        sb.append("<head>");
+        sb.append("<title> say-hello-using-html</title>");
+        sb.append("</head>");
+        sb.append("<body>");
+        sb.append("<h2>This is my body..!</h2>");
+        sb.append("</body>");
+        sb.append("</html>");
+
+        return sb.toString();
     }
 
 }
