b2a8a8de578577561948eb00a98a9dfa00d2368d
Aditya-Prakash07 <prakashaditya775@gmail.com>
2025-01-24 15:15:03 +0530

    jsp page implemented successfully now i can display my html page using jsp in a springboot application

--- src/main/java/com/aditya/springboot/todomanagementwebapp/hello/SayHelloController.java
+++ src/main/java/com/aditya/springboot/todomanagementwebapp/hello/SayHelloController.java
@@ -5,20 +5,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
-@Controller // this annotaion is for showing that below class is something that handles web-request
-//public class SayHelloController {
-//    //"say-hello" => "Hello! What are you learning today?"
-//    //say-hello ---> i want that when someone clicks on my url named say-hello he will get the above message
-//    // url ==>  http://localhost:8080/say-hello
-//    @RequestMapping("say-hello") // map to url
-//    @ResponseBody // using this annotation we are sending the string returned here as response
-//    public String sayHello(){
-//        return "Hello! What are you learning today?";
-//    }
-//
-//}
-
+@Controller // this annotaion is for showing that below class is something that handles
 public class SayHelloController {
+   // "say-hello" => "Hello! What are you learning today?"
+    //say-hello ---> i want that when someone clicks on my url named say-hello he will get the above message
+    // url ==>  http://localhost:8080/say-hello
+    @RequestMapping("say-hello") // map to url
+    @ResponseBody // using this annotation we are sending the string returned here as response
+    public String sayHello(){
+        return "Hello! What are you learning today?";
+    }
+
+
+
     //"say-hello" => "Hello! What are you learning today?"
     //say-hello ---> i want that when someone clicks on my url named say-hello he will get the above message
     // url ==>  http://localhost:8080/say-hello
@@ -37,5 +36,21 @@ public class SayHelloController {
 
         return sb.toString();
     }
+    // Showing the html page as the above page is very very hectic so we need to use some kind of view
+    // technology like JSP(it is one of the earliest and popular view technology).
+    // when someone goes to the url say-hello-jsp ===> we want to redirect them to sayHello.jsp page
 
+
+    //src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
+    //src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
+    //src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+    //src/main/resources/META-INF/resources/WEB-INF/jsp/todos.jsp
+// we will be making these pages what we can see here is only name of our jsp pages is canging
+// so we can store our constant part in apllication properties as prefix and suffix
+    @RequestMapping("say-hello-jsp") // map to url
+    //@ResponseBody // using this annotation we are sending the string returned here as response so
+    // for returning the sayHello.jsp page just remove the @ResponseBody annotation
+    public String sayHelloJsp(){
+        return "sayHello";
+    }
 }
---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <title>html page using .jsp page file</title>
+    </head>
+    <body>
+        <h1>Hello I am html page made using .jsp page and by using java and spring boot</h1>
+    </body>
+</html>
