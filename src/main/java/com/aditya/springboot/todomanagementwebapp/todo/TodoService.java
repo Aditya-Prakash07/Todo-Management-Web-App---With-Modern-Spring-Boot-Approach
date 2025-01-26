ef7955443ed57928c963a172145ede34aeb87e9a
Aditya-Prakash07 <prakashaditya775@gmail.com>
2025-01-27 01:03:59 +0530

    jsp page formatting with bootstrap and implementing feature to add new todo

--- src/main/java/com/aditya/springboot/todomanagementwebapp/todo/TodoService.java
+++ src/main/java/com/aditya/springboot/todomanagementwebapp/todo/TodoService.java
@@ -9,13 +9,20 @@ import java.util.List;
 @Service
 public class TodoService {
     private static List<Todo> todos = new ArrayList<>();
+    public static int todosCount = 0;
     static{
-        todos.add(new Todo(1, "Aditya", "Lean Devops", LocalDate.now().plusYears(1),false));
-        todos.add(new Todo(1, "Aditya", "Lean Devops", LocalDate.now().plusYears(1),false));
-        todos.add(new Todo(1, "Aditya", "Lean Devops", LocalDate.now().plusYears(1),false));
-        todos.add(new Todo(1, "Aditya", "Lean Devops", LocalDate.now().plusYears(1),false));
+        todos.add(new Todo(++todosCount, "Aditya", "Lean Devops", LocalDate.now().plusYears(1),false));
+        todos.add(new Todo(++todosCount, "Aditya", "Lean Devops", LocalDate.now().plusYears(1),false));
+        todos.add(new Todo(++todosCount, "Aditya", "Lean Devops", LocalDate.now().plusYears(1),false));
+        todos.add(new Todo(++todosCount, "Aditya", "Lean Devops", LocalDate.now().plusYears(1),false));
     }
     public List<Todo> findByUsername(String username){
         return todos;
     }
+    public void addTodo(String username, String description, LocalDate targetDate, boolean done){
+        Todo todo = new Todo(++todosCount, username, description, targetDate, done );
+        todos.add(todo);
+
+    }
+
 }
--- src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
@@ -3,11 +3,9 @@
         <title>Login Page</title>
     </head>
     <body>
-
-
-        <h4>Welcome to welcome.jsp page!</h4>
-        <div>Welcome ${name}</div>
-        <div><a href="list-todos">Manage</a> your todos</div>
-       <!-- <div>Your password is : ${password}</div> -->
+		<div class="container">
+			<h1>Welcome ${name}</h1>
+			<a href="list-todos">Manage</a> your todos
+		</div>
     </body>
 </html>
