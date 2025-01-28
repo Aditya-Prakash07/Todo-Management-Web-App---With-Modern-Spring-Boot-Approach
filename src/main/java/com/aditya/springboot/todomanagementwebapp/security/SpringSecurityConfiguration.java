40f7f6223ab2c105f0a88ad0a0ef61f77551fc74
Aditya-Prakash07 <prakashaditya775@gmail.com>
2025-01-28 12:37:00 +0530

    removed the deprecated method withDefaultPasswordEncoder() and user User.bulilder() insted

--- src/main/java/com/aditya/springboot/todomanagementwebapp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/aditya/springboot/todomanagementwebapp/security/SpringSecurityConfiguration.java
@@ -4,20 +4,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SpringSecurityConfiguration {
-    // For storing user name and password we should make use of LDAP or at leat a database
-    // to keep things simple we will use here in memory configurer
-    //InMemoryUserDetailsManager
-    //InMemoryUserDetailsManager(UserDetails... users)
+    // For storing username and password, it is recommended to use LDAP or a database.
+    // To keep things simple, we will use in-memory configuration here.
 
     @Bean
     public InMemoryUserDetailsManager createUserDetailsManager() {
         // Create a user with username, password, and roles
-        UserDetails userDetails = User.withDefaultPasswordEncoder()
+        UserDetails userDetails = User.builder()
                 .username("adi")
+                .passwordEncoder(passwordEncoder()::encode) // Pass the encoder reference directly
                 .password("dummy")
                 .roles("USER", "ADMIN")
                 .build();
@@ -25,5 +26,11 @@ public class SpringSecurityConfiguration {
         // Return InMemoryUserDetailsManager with the created user
         return new InMemoryUserDetailsManager(userDetails);
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
+
 
